1aa1e8fdc5215b3e8b8779f659f0e1243e290bd8
Izzy George <10izzygeorge@gmail.com>
2016-04-21 18:41:38 -0700

    display user input on results page

--- app/src/main/java/com/isabellegeorge/pinballwizard/ResultsActivity.java
+++ app/src/main/java/com/isabellegeorge/pinballwizard/ResultsActivity.java
@@ -1,19 +1,24 @@
 package com.isabellegeorge.pinballwizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ResultsActivity extends AppCompatActivity {
+    @Bind(R.id.resultsTextView) TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-
+        Intent i = getIntent();
+        String location = i.getStringExtra("location");
+        mLocationTextView.setText("Pinball near: " + location);
     }
 }
