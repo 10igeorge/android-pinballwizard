3f2c16f493b8a7f72841a20f1c94c766a61f72c5
Izzy George <10izzygeorge@gmail.com>
2016-04-21 18:20:07 -0700

    create splash screen

---
+++ app/src/main/java/com/isabellegeorge/pinballwizard/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.isabellegeorge.pinballwizard;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
