27c9e5d5a927fbf5466e9a8031c59f837b2315cb
Izzy George <10izzygeorge@gmail.com>
2016-04-22 09:52:34 -0700

    create flipper vectors for splash screen

--- app/src/main/java/com/isabellegeorge/pinballwizard/SplashActivity.java
+++ app/src/main/java/com/isabellegeorge/pinballwizard/SplashActivity.java
@@ -5,12 +5,16 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        ButterKnife.bind(this);
 
         new Handler().postDelayed(new Runnable() {
             @Override
