8c1ac9f8d183cfc6ef6f0de605b2ff0dd4f1e3e7
Izzy George <10izzygeorge@gmail.com>
2016-04-21 18:48:55 -0700

    list hard coded locations with pinball on results activity

--- app/src/main/java/com/isabellegeorge/pinballwizard/ResultsActivity.java
+++ app/src/main/java/com/isabellegeorge/pinballwizard/ResultsActivity.java
@@ -4,21 +4,43 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ResultsActivity extends AppCompatActivity {
-    @Bind(R.id.resultsTextView) TextView mLocationTextView;
+    @Bind(R.id.resultsTextView) TextView mResultsTextView;
+    @Bind(R.id.locationsWithPinball) ListView mLocations;
+    String[] hardCodedLocations = new String[] {
+            "Ground Kontrol",
+            "Scoreboard",
+            "Momo",
+            "Kelly's Olympian",
+            "Yamhill Pub",
+        };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, hardCodedLocations);
+        mLocations.setAdapter(adapter);
+
+
+
+
+
+
+
         Intent i = getIntent();
         String location = i.getStringExtra("location");
-        mLocationTextView.setText("Pinball near: " + location);
+        mResultsTextView.setText("Pinball near: " + location);
     }
 }
