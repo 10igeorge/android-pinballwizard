c01630c7c27f5ff91f290b34c3ed88063a29482f
Izzy George <10izzygeorge@gmail.com>
2016-04-22 10:51:44 -0700

    create toast message on Main Activity

--- app/src/main/java/com/isabellegeorge/pinballwizard/MainActivity.java
+++ app/src/main/java/com/isabellegeorge/pinballwizard/MainActivity.java
@@ -1,11 +1,15 @@
 package com.isabellegeorge.pinballwizard;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,12 +17,26 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.locationEditText) EditText mLocation;
     @Bind(R.id.searchRegionButton) Button mSearchRegion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         mSearchRegion.setOnClickListener(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.instructions_toast,
+                (ViewGroup) findViewById(R.id.toast_layout_root));
+
+        TextView mToastText = (TextView) layout.findViewById(R.id.toastText);
+        mToastText.setText("");
+
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+
     }
 
     @Override
