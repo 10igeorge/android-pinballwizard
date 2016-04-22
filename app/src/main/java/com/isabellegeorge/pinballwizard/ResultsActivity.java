80861173e569beaeb016883c5a352541a4cb4447
Izzy George <10izzygeorge@gmail.com>
2016-04-22 14:29:35 -0700

    create a spinner to filter by location or machine

--- app/src/main/java/com/isabellegeorge/pinballwizard/ResultsActivity.java
+++ app/src/main/java/com/isabellegeorge/pinballwizard/ResultsActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,10 +20,11 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ResultsActivity extends AppCompatActivity {
+public class ResultsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Bind(R.id.resultsTextView) TextView mResultsTextView;
     @Bind(R.id.locationsWithPinball) ListView mLocations;
     @Bind(R.id.filterSpinner) Spinner mFilter;
+
     String[] hardCodedLocations = new String[] {
             "Ground Kontrol",
             "Scoreboard",
@@ -28,7 +32,16 @@ public class ResultsActivity extends AppCompatActivity {
             "Kelly's Olympian",
             "Yamhill Pub",
         };
-    String[] filters = new filters[] {
+
+    String[] hardCodedMachines = new String[] {
+            "Ground Kontrol",
+            "Scoreboard",
+            "Momo",
+            "Kelly's Olympian",
+            "Yamhill Pub",
+    };
+
+    String[] filters = new String[] {
             "Locations",
             "Machines"
         };
@@ -40,15 +53,29 @@ public class ResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_results);
         ButterKnife.bind(this);
 
-        mFilter.setOnItemSelectedListener(this);
+        mFilter.setOnItemSelectedListener(ResultsActivity.this);
+
         List<String> spinnerFilters = new ArrayList<>(Arrays.asList(filters));
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinnerFilters);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mFilter.setAdapter(spinnerAdapter);
 
+        ArrayAdapter listAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, hardCodedLocations);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, hardCodedLocations);
-        mLocations.setAdapter(adapter);
+        mLocations.setAdapter(listAdapter);
 
         Intent i = getIntent();
         String location = i.getStringExtra("location");
-        mResultsTextView.setText("Pinball near: " + location);
+        mResultsTextView.setText("Pinball near " + location);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected " + item, Toast.LENGTH_LONG).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0){
+        //do nothing
     }
 }
