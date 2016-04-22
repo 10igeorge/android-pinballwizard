e6a05f5250a8b9b69b17b114746e1d847c6701cf
Izzy George <10izzygeorge@gmail.com>
2016-04-22 10:08:07 -0700

    revamp splash screen

--- app/src/main/java/com/isabellegeorge/pinballwizard/SplashActivity.java
+++ app/src/main/java/com/isabellegeorge/pinballwizard/SplashActivity.java
@@ -1,13 +1,17 @@
 package com.isabellegeorge.pinballwizard;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+    @Bind(R.id.splashLogo) TextView mSplashLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,9 @@ public class SplashActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        Typeface monoton = Typeface.createFromAsset(getAssets(), "fonts/Monoton-Regular.ttf");
+        mSplashLogo.setTypeface(monoton);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,6 +30,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        }, 3000);
+        }, 4000);
     }
 }
