e557d9bf416704eec7bef0a4fa2bf0899cf9cadc
Izzy George <10izzygeorge@gmail.com>
2016-04-22 17:36:52 -0700

    pass instrumentation test

---
+++ app/src/androidTest/java/com/isabellegeorge/pinballwizard/ResultsActivityInstrumentationTest.java
@@ -0,0 +1,30 @@
+package com.isabellegeorge.pinballwizard;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+public class ResultsActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<ResultsActivity> activityTestRule =
+            new ActivityTestRule<>(ResultsActivity.class);
+
+    @Test
+    public void spinnerSelect() {
+        onView(withId(R.id.filterSpinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Machines"))).perform(click());
+        onView(withId(R.id.filterSpinner)).check(matches(withSpinnerText("Machines")));
+    }
+}
