d9b86f3eb2d17f9fd8853493c02f5815351eb15d
Izzy George <10izzygeorge@gmail.com>
2016-04-22 17:01:55 -0700

    pass instrumentation tests

--- app/src/androidTest/java/com/isabellegeorge/pinballwizard/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/isabellegeorge/pinballwizard/MainActivityInstrumentationTest.java
@@ -7,10 +7,9 @@ import org.junit.Test;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static java.util.regex.Pattern.matches;
-import static org.hamcrest.Matchers.containsString;
 
 public class MainActivityInstrumentationTest {
 
@@ -19,18 +18,18 @@ public class MainActivityInstrumentationTest {
             new ActivityTestRule<>(MainActivity.class);
 
 //TODO: figure out why withText method doesn't work
-//    @Test
-//    public void validateEditText() {
-//        onView(withId(R.id.locationEditText)).perform(typeText("97204"))
-//                .check(matches(withText("97204")));
-//    }
-//
-//    @Test
-//    public void zipCodeIsSentToResultsActivity() {
-//        String location = "78704";
-//        onView(withId(R.id.locationEditText)).perform(typeText(location));
-//        onView(withId(R.id.searchRegionButton)).perform(click());
-//        onView(withId(R.id.locationsWithPinball)).check(matches
-//                (withText("Pinball near " + location)));
-//    }
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("97204"))
+                .check(matches(withText("97204")));
+    }
+
+    @Test
+    public void zipCodeIsSentToResultsActivity() {
+        String location = "78704";
+        onView(withId(R.id.locationEditText)).perform(typeText(location));
+        onView(withId(R.id.searchRegionButton)).perform(click());
+        onView(withId(R.id.locationsWithPinball)).check(matches
+                (withText("Pinball near " + location)));
+    }
 }
