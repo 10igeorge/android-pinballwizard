561179d7f228d81316e74bd758b3e1d3412d9348
Izzy George <10izzygeorge@gmail.com>
2016-05-05 10:10:42 -0700

    create a new account page

---
+++ app/src/main/java/com/isabellegeorge/pinballwizard/models/User.java
@@ -0,0 +1,20 @@
+package com.isabellegeorge.pinballwizard.models;
+
+/**
+ * Created by Guest on 5/5/16.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public User(){}
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() { return name; }
+
+    public String getEmail() { return email; }
+}
