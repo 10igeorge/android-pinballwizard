0351bc7a1f805b3675856cf887828705ebd1aa58
10igeorge <10izzygeorge@gmail.com>
2016-05-08 14:25:17 -0700

    implement firebase adapter to reuse recycler view for saved locations

--- app/src/main/java/com/isabellegeorge/pinballwizard/ui/NavDrawerActivity.java
+++ app/src/main/java/com/isabellegeorge/pinballwizard/ui/NavDrawerActivity.java
@@ -2,10 +2,13 @@ package com.isabellegeorge.pinballwizard.ui;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.app.FragmentManager;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,9 +20,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.isabellegeorge.pinballwizard.Constants;
 import com.isabellegeorge.pinballwizard.R;
+import com.isabellegeorge.pinballwizard.models.User;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -28,15 +35,23 @@ public class NavDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
         protected DrawerLayout drawer;
         private Firebase ref;
+        private SharedPreferences mSharedPreferences;
+        private TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        TextView username = (TextView) findViewById(R.id.userName);
+        username = (TextView) findViewById(R.id.userName);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ref = new Firebase(Constants.FIREBASE_URL);
+        ref = new Firebase(Constants.FIREBASE_URL_USERS);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String uid = mSharedPreferences.getString(Constants.KEY_UID, null);
+        ref = new Firebase(Constants.FIREBASE_URL_USERS + "/" + uid);
+
+
+
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -56,6 +71,21 @@ public class NavDrawerActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        username = (TextView) header.findViewById(R.id.userName);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText("@"+user.getName());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -93,12 +123,8 @@ public class NavDrawerActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-// Handle navigation view item clicks here.ks here.
-
-
-
+    // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Fragment fragment = null;
         if (id == R.id.nav_locations) {
             Intent i = new Intent(this, SavedLocationsActivity.class);
             startActivity(i);
@@ -128,6 +154,5 @@ public class NavDrawerActivity extends AppCompatActivity
 
 
 //TODO: Search widget?
-//TODO: List user's saved locations
 //TODO: Search by machines
 //TODO: Change location
